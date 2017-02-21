59d36e65900021c197a49df151c4a210b30aa239
quanghung97 <hungcan1997@gmail.com>
2017-02-21 18:25:50 +0700

    fix ten thu muc controller

--- app/Http/Controllers/de_tai_du_an_cac_cap/DetailController.php
+++ app/Http/Controllers/de_tai_du_an_cac_cap/DetailController.php
@@ -1,11 +1,14 @@
 <?php
 
-namespace App\Http\Controllers\de-tai-du-an-cac-cap;
+namespace App\Http\Controllers\de_tai_du_an_cac_cap;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+
 class DetailController extends Controller
 {
-    //
+    public function a(){
+        echo "ets";
+    }
 }
