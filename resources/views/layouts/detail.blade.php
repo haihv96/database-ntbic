d483c0b6c8c66b542300601f3ac7600da66e8125
hoanghai096 <hai.hp.96@gmail.com>
2017-02-21 13:24:43 +0700

    first commit

---
+++ resources/views/layouts/detail.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<div class="header">
+		{{-- code --}}
+	</div>
+	<div class="search-option">
+		@yield('search-option')
+		Phần này chứa thanh search và các lựa chọn nhỏ khi search. đã được khoanh vùng đen ở Requirement
+	</div>
+	<div class="main">
+		@yield('main')
+	</div>
+	<div class="sidebar">
+		@yield('sidebar')
+	</div>
+</body>
+</html>
