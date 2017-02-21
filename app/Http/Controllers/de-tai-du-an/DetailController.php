ce21cc90f1744812f2f1ea4e5a3c8f0f9b1de0e9
hoanghai096 <hai.hp.96@gmail.com>
2017-02-21 13:46:07 +0700

    create folder controller

---
+++ app/Http/Controllers/de-tai-du-an/DetailController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\de-tai-du-an;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class DetailController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/phat-minh/DetailController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\phat-minh;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class DetailController extends Controller
+{
+    //
+}
