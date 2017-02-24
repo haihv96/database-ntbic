901f4cc332468255207df2e86442ef5bf847a6ea
quanghung97 <hungcan1997@gmail.com>
2017-02-24 16:04:46 +0700

    update chuyen_gia

--- app/Http/Controllers/chuyen_gia/DetailController.php
+++ app/Http/Controllers/chuyen_gia/DetailController.php
@@ -9,16 +9,30 @@ use Illuminate\Support\Facades\DB;
 class DetailController extends Controller
 {
     
-    //về phần chuyên gia khcn mô hình CSDL em thấy chưa rõ anh Hải xem giúp em nhé
-    //vì chuyên gia có người có người thì không có công trình nghiên cứu hoặc kết quả nghiên cứu có thể anh cân nhắc tính chuyện này có nên thêm hàm mới để lấy dữ liệu 2 bảng kia hay là cùng hiển thị hết trên 1 trang 
+    
     protected function index($link)
     {
-        $data = DB::table('chuyen_gia_khcn')->select('ho_va_ten','hoc_vi','nam_sinh','chuyen_nganh','co_quan','huong_nghien_cuu','link_anh','Sl_congTrinh_baiBao','tinh_thanh')->where('linkid',$link)->get();
-       
+        /*
+            $data_chinh dữ liệu trong bảng chuyen_gia_khcn
+            $data_phu1 dữ liệu cong_trinh_nghien_cuu của chuyên gia đó 
+            $data_phu2 dữ liệu ket_qua_nghien_cuu của chuyên gia đó 
+        */
+        $data_chinh = DB::table('chuyen_gia_khcn')->select('ho_va_ten','hoc_vi','nam_sinh','chuyen_nganh','co_quan','huong_nghien_cuu','link_anh','Sl_congTrinh_baiBao','tinh_thanh')->where('linkid',$link)->get();
+        
+        $data_phu1 = DB::table('chuyen_gia_khcn')->join('cong_trinh_nghien_cuu','cong_trinh_nghien_cuu.id_profile','=','chuyen_gia_khcn.id')->select('cong_trinh_nghien_cuu.content')->where('linkid',$link)->get();
+        
+        $data_phu2 = DB::table('chuyen_gia_khcn')->join('ket_qua_nghien_cuu','ket_qua_nghien_cuu.id_profile','=','chuyen_gia_khcn.id')->select('ket_qua_nghien_cuu.content')->where('linkid',$link)->get();
+        
+        
+        
+        
         echo "<pre>";
-        print_r($data);
+        print_r($data_chinh);
+        print_r($data_phu1);
+        print_r($data_phu2);
         //echo $data[0]->huong_nghien_cuu;// mảng gồm các đối tượng có thể gắn biến sau nếu cần 
         echo "</pre>";
+        
     }
     
 }
