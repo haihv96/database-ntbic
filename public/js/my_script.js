8d9e803fd66d6b7006b8d303f21bc6133ee17c38
hoanghai096 <hai.hp.96@gmail.com>
2017-03-04 08:40:49 +0700

    push

--- public/js/my_script.js
+++ public/js/my_script.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
 	$("#search_home").click(function(){
-		var url = window.location.href +$('input[name=type-search]:checked').val() + '/?text_search='+ $("#text_search").val();
-		window.location.replace(url);
+		window.location.replace('/database-ntbic/' + $('input[name=type-search]:checked').val() + '/?text_search='+ $("#text_search").val());
 	});
-	$()
 });
