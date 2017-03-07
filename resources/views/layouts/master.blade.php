ef9f857fa2a09a4a81eebde2c133ee9fa007b4e9
hoanghai096 <hai.hp.96@gmail.com>
2017-03-07 20:09:09 +0700

    s

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -20,7 +20,7 @@
 		      <button type="button" class="navbar-toggle menu-hidden-btn" data-toggle="collapse" data-target="#myNavbar">
 		        <span class="glyphicon glyphicon-align-justify"></span>                      
 		      </button>
-		      <a class="navbar-brand" href="/"><span class="logo">NTBIC</span></a>
+		      <a class="navbar-brand" href="{{ URL::asset('/') }}"><span class="logo">NTBIC</span></a>
 		    </div>
 		    <div class="collapse navbar-collapse" id="myNavbar">
 		      <ul class="nav navbar-nav navbar-right">
