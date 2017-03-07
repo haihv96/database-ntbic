72b19a569008f8486bae31343f41f1bf04905499
hoanghai096 <hai.hp.96@gmail.com>
2017-03-07 20:00:47 +0700

    a

--- public/js/my_script.js
+++ public/js/my_script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
 	$("#search_home").click(function(){
-		window.location.replace('/database-ntbic/' + $('input[name=type-search]:checked').val() + '/?text_search='+ $("#text_search").val());
+		window.location.replace('/database-ntbic/' + $('input[name=type-search]:checked').val() + '?text_search='+ $("#text_search").val());
 	});
 });
