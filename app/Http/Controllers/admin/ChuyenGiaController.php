e31d283494c6a9d7bb0dfa55769e61a990439752
hoanghai096 <hai.hp.96@gmail.com>
2017-03-11 09:49:08 +0700

    a

--- app/Http/Controllers/admin/ChuyenGiaController.php
+++ app/Http/Controllers/admin/ChuyenGiaController.php
@@ -8,6 +8,6 @@ use App\Http\Controllers\Controller;
 class ChuyenGiaController extends Controller
 {
     public function index(){
-    	return view('admin.chuyen_gia.')
+    	return view('admin.chuyen_gia.index');
     }
 }
