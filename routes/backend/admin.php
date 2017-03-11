e51233f2e9beaf7ec446aef42e247cb5023f96a9
hoanghai096 <hai.hp.96@gmail.com>
2017-03-11 10:38:29 +0700

    a

--- routes/backend/admin.php
+++ routes/backend/admin.php
@@ -4,45 +4,45 @@
 	Route::group(['prefix'=>'admin'], function(){
 		Route::group(['prefix'=>'chuyen-gia'], function(){
 			Route::get('/','admin\ChuyenGiaController@index');
-			Route::get('tao-moi','admin\ChuyenGiaController@new');
-			Route::get('xoa','admin\ChuyenGiaController@delete');
-			Route::get('sua','admin\ChuyenGiaController@edit');
+			Route::get('tao-moi','admin\ChuyenGiaController@new_index');
+			Route::get('xoa','admin\ChuyenGiaController@delete_index');
+			Route::get('sua','admin\ChuyenGiaController@edit_index');
 			Route::post('tao-moi','admin\ChuyenGiaController@new_action');
 			Route::post('sua','admin\ChuyenGiaController@edit_action');
 		});
 
 		Route::group(['prefix'=>'doanh-nghiep'], function(){
 			Route::get('/','admin\DoanhNghiepController@index');
-			Route::get('tao-moi','admin\DoanhNghiepController@new');
-			Route::get('xoa','admin\DoanhNghiepController@delete');
-			Route::get('sua','admin\DoanhNghiepController@edit');
+			Route::get('tao-moi','admin\DoanhNghiepController@new_index');
+			Route::get('xoa','admin\DoanhNghiepController@delete_index');
+			Route::get('sua','admin\DoanhNghiepController@edit_index');
 			Route::post('tao-moi','admin\DoanhNghiepController@new_action');
 			Route::post('sua','admin\DoanhNghiepController@edit_action');
 		});
 
 		Route::group(['prefix'=>'san-pham'], function(){
 			Route::get('/','admin\SanPhamController@index');
-			Route::get('tao-moi','admin\SanPhamController@new');
-			Route::get('xoa','admin\SanPhamController@delete');
-			Route::get('sua','admin\SanPhamController@edit');
+			Route::get('tao-moi','admin\SanPhamController@new_index');
+			Route::get('xoa','admin\SanPhamController@delete_index');
+			Route::get('sua','admin\SanPhamController@edit_index');
 			Route::post('tao-moi','admin\SanPhamController@new_action');
 			Route::post('sua','admin\SanPhamController@edit_action');
 		});
 
 		Route::group(['prefix'=>'de-tai-du-an-cac-cap'], function(){
 			Route::get('/','admin\DtDaCacCapController@index');
-			Route::get('tao-moi','admin\DtDaCacCapController@new');
-			Route::get('xoa','admin\DtDaCacCapController@delete');
-			Route::get('sua','admin\DtDaCacCapController@edit');
+			Route::get('tao-moi','admin\DtDaCacCapController@new_index');
+			Route::get('xoa','admin\DtDaCacCapController@delete_index');
+			Route::get('sua','admin\DtDaCacCapController@edit_index');
 			Route::post('tao-moi','admin\DtDaCacCapController@new_action');
 			Route::post('sua','admin\DtDaCacCapController@edit_action');
 		});
 
 		Route::group(['prefix'=>'phat-minh-sang-che'], function(){
 			Route::get('/','admin\PhatMinhController@index');
-			Route::get('tao-moi','admin\PhatMinhController@new');
-			Route::get('xoa','admin\PhatMinhController@delete');
-			Route::get('sua','admin\PhatMinhController@edit');
+			Route::get('tao-moi','admin\PhatMinhController@new_index');
+			Route::get('xoa','admin\PhatMinhController@delete_index');
+			Route::get('sua','admin\PhatMinhController@edit_index');
 			Route::post('tao-moi','admin\PhatMinhController@new_action');
 			Route::post('sua','admin\PhatMinhController@edit_action');
 		});
