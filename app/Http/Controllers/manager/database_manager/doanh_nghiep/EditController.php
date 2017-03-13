d4d4c210c41ec93cfcbba8bf15068f6516eb2523
Dai Nguyen <dainv.1710@gmail.com>
2017-03-13 22:57:15 +0700

    index doanh nghiệp

--- app/Http/Controllers/manager/database_manager/doanh_nghiep/EditController.php
+++ app/Http/Controllers/manager/database_manager/doanh_nghiep/EditController.php
@@ -4,8 +4,12 @@ namespace App\Http\Controllers\manager\database_manager\doanh_nghiep;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\doanh_nghiep_khcn;
 
 class EditController extends Controller
 {
-    //
+    public function index($id) {
+    	$doanh_nghiep = doanh_nghiep_khcn::find($id);
+    	return view('database_manager.doanh_nghiep.edit')->with(['doanh_nghiep'=>$doanh_nghiep]);
+    }
 }
