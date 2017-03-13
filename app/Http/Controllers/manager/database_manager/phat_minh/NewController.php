93a1f6dccbe8980b30d86033f1683610abc4a09d
truongnhung <truongnhung367@gmail.com>
2017-03-13 23:38:48 +0700

    a

--- app/Http/Controllers/manager/database_manager/phat_minh/NewController.php
+++ app/Http/Controllers/manager/database_manager/phat_minh/NewController.php
@@ -7,5 +7,8 @@ use App\Http\Controllers\Controller;
 
 class NewController extends Controller
 {
-    //
+    public function index()
+    {
+    	return view('database_manager.phat_minh.new');
+    }
 }
